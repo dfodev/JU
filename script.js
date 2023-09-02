fc2951e4a2078b3dfb33cc0f840c2a5b27aba856
DanielF Origamid <dev.danieloliveira@gmail.com>
2023-09-01 22:27:09 -0400

    atualização feita

--- script.js
+++ script.js
@@ -1,42 +1,3 @@
-// const simButtons = document.querySelectorAll('.sim-button');
-// const naoButton = document.querySelector('.nao-button');
-// const h2 = document.querySelector('h2');
-// let clickCount = 0;
-
-// simButtons.forEach(function (simButton) {
-//   simButton.addEventListener('click', function () {
-//     clickCount++;
-
-//     if (clickCount === 1) {
-//       h2.textContent = 'Vamos namorar?';
-//     } else if (clickCount === 2) {
-//       h2.textContent =
-//         'Aceita namorar comigo e me provocar cada vez mais sorrisos?';
-//     } else if (clickCount === 3) {
-//       h2.textContent = 'UUUUUHHHUUUUUUUUUUU🥰🥰😍😍 ';
-//       // Após a terceira vez que "Sim" é clicado, aguarde 2 segundos e, em seguida, abra o link
-//       setTimeout(function () {
-//         window.location.href = 'https://www.youtube.com'; // Substitua pelo link desejado
-//       }, 2000); // 2000 milissegundos = 2 segundos
-//     }
-
-//     // Reposicionar o botão "Não" ao lado dos botões "Sim"
-//     naoButton.style.left = 'auto'; // Limpar a posição "left"
-//   });
-// });
-
-// function desvia(t) {
-//   var naoButton = t;
-//   naoButton.style.position = 'absolute';
-//   naoButton.style.bottom = geraPosicao(10, 70);
-//   naoButton.style.left = geraPosicao(10, 70);
-//   console.log('opa, desviei...');
-// }
-
-// function geraPosicao(min, max) {
-//   return Math.random() * (max - min) + min + '%';
-// }
-
 const simButtons = document.querySelectorAll('.sim-button');
 const naoButton = document.querySelector('.nao-button');
 const h2 = document.querySelector('h2');
@@ -48,10 +9,10 @@ simButtons.forEach(function (simButton) {
     clickCount++;
 
     if (clickCount === 1) {
-      h2.textContent = 'Vamos namorar?';
+      h2.textContent = 'Me te tirar do sério?';
     } else if (clickCount === 2) {
       h2.textContent =
-        'Aceita namorar comigo e me provocar cada vez mais sorrisos?';
+        'Aceita namorar comigo e me provocar a ter os melhores sorrisos com voce?';
     } else if (clickCount === 3) {
       h2.textContent = 'UUUUUHHHUUUUUUUUUUU🥰🥰😍😍 ';
       // Após a terceira vez que "Sim" é clicado, aguarde 2 segundos e, em seguida, abra o link
